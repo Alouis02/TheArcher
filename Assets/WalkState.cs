ad1bcdbe7a354b443e06fc3d37009ad8d8b65a28
Alouis02 <163452952+Alouis02@users.noreply.github.com>
2025-04-17 12:13:02 -0400

    new

--- Assets/WalkState.cs
+++ Assets/WalkState.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
+using UnityEngine.AI;
+using System.Collections.Generic;
 
 public class WalkState : StateMachineBehaviour
 {
     float timer;
+    List<Transform> waypoints = new List<Transform>();
+    NavMeshAgent agent;
+
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        agent = animator.GetComponent<NavMeshAgent>();
         timer = 0;
+        GameObject go = GameObject.FindWithTag("WayPoints");
+        foreach (Transform t in go.transform) {
+            waypoints.Add(t);
+        }
+        agent.SetDestination(waypoints[Random.Range(0, waypoints.Count)].position);
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        if (agent.remainingDistance <= agent.stoppingDistance) {
+            agent.SetDestination(waypoints[Random.Range(0, waypoints.Count)].position);
+        }
         timer += Time.deltaTime;
         if (timer > 10) {
             animator.SetBool("IsPatrolling", false);
@@ -21,7 +35,7 @@ public class WalkState : StateMachineBehaviour
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+        agent.SetDestination(agent.transform.position);
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
