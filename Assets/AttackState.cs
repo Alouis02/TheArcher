8fd20a00570186d9befa6ac3a52cee9c0a200a9b
Alouis02 <163452952+Alouis02@users.noreply.github.com>
2025-04-17 16:59:31 -0400

    updated

--- Assets/AttackState.cs
+++ Assets/AttackState.cs
@@ -2,28 +2,22 @@ using UnityEngine;
 using UnityEngine.AI;
 using System.Collections.Generic;
 
-public class IdleState : StateMachineBehaviour
+public class AttackState : StateMachineBehaviour
 {
-    float timer;
     Transform player;
-    float range = 8;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        timer = 0;
         player = GameObject.FindWithTag("Player").transform;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        timer += Time.deltaTime;
-        if (timer > 5) {
-            animator.SetBool("IsPatrolling", true);
-        }
+        animator.transform.LookAt(player);
         float distance = Vector3.Distance(player.position, animator.transform.position);
-        if (distance < range) {
-            animator.SetBool("IsChasing", true);
+        if (distance > 2.5f) {
+            animator.SetBool("IsAttacking", false);
         }
     }
 
--- Assets/WalkState.cs
+++ Assets/WalkState.cs
@@ -7,11 +7,14 @@ public class WalkState : StateMachineBehaviour
     float timer;
     List<Transform> waypoints = new List<Transform>();
     NavMeshAgent agent;
-
+    Transform player;
+    float range = 8;
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        player = GameObject.FindWithTag("Player").transform;
         agent = animator.GetComponent<NavMeshAgent>();
+        agent.speed = 1.5f;
         timer = 0;
         GameObject go = GameObject.FindWithTag("WayPoints");
         foreach (Transform t in go.transform) {
@@ -29,6 +32,10 @@ public class WalkState : StateMachineBehaviour
         timer += Time.deltaTime;
         if (timer > 10) {
             animator.SetBool("IsPatrolling", false);
+        }
+        float distance = Vector3.Distance(player.position, animator.transform.position);
+        if (distance < range) {
+            animator.SetBool("IsChasing", true);
         }
     }
 
